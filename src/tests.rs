682ae80419d6133454c0caf11d45e22cfae3837d
laralove <laralove143@icloud.com>
2022-10-17 18:13:22 +0300

    Ignore fields in current user that are only sent in HTTP requests

--- src/tests.rs
+++ src/tests.rs
@@ -500,14 +500,14 @@ impl<T: Cache + Send + Sync> Tester<T> {
         Ok(())
     }
 
-    /// Updates the cache with the pending events for 5 seconds
+    /// Updates the cache with the pending events for 1 second
     async fn update(&mut self) -> Result<(), anyhow::Error> {
         let started = Instant::now();
 
         while let Some(event) = self.events.next().await {
             self.cache.update(&event).await?;
 
-            if started.elapsed().as_secs() > 5 {
+            if started.elapsed().as_secs() > 1 {
                 return Ok(());
             }
         }
@@ -542,7 +542,10 @@ impl<T: Cache + Send + Sync> Tester<T> {
     async fn assert_current_users_eq(&mut self) -> Result<(), anyhow::Error> {
         self.update().await?;
 
-        let current_user = self.http.current_user().exec().await?.model().await?;
+        let mut current_user = self.http.current_user().exec().await?.model().await?;
+        current_user.locale = None;
+        current_user.premium_type = None;
+        current_user.public_flags = None;
         assert_eq!(self.cache.current_user().await?, current_user);
 
         Ok(())
